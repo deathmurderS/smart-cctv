6b08fb2b5b5ad77e85e91c64bd8372dcf4388ee0
Zaky <zakychen558@gmail.com>
2026-05-13 09:58:48 +0700

    fix: remove socket.io from frontend

--- frontend/src/hooks/useSocket.js
+++ frontend/src/hooks/useSocket.js
@@ -1,20 +1,5 @@
-import { useEffect, useState } from 'react'
-import { io } from 'socket.io-client'
-
-const socket = io('http://localhost:3000')
-
 const useSocket = () => {
-    const [lastEvent, setLastEvent] = useState(null)
-
-    useEffect(() => {
-        socket.on('new_event', (event) => {
-            setLastEvent(event)
-        })
-
-        return () => socket.off('new_event')
-    }, [])
-
-    return { lastEvent }
+    return { lastEvent: null }
 }
 
 export default useSocket
