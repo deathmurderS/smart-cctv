10711f7f41f3bcfbe879cc1cda2823c808624e16
Zaky <zakychen558@gmail.com>
2026-05-15 16:41:55 +0700

    fix: capitalize useRef import

--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useref } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
